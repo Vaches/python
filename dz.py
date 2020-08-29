3ed86eb91872b2a62234c4646889b7e8350a6702
Vaches <70432569+Vaches@users.noreply.github.com>
2020-08-29 17:00:15 +0300

    Add files via upload

---
+++ dz.py
@@ -0,0 +1,18 @@
+#work 3
+#a = int(input ('введите месяц '))
+#if a == 1 or a == 2 or a == 12:
+ #   print ('зима')
+#elif a == 3 or a == 4 or a == 5:
+#    print ('весна')
+#elif a == 6 or a == 7 or a == 8:
+ #   print ('лето')
+#elif a == 9 or a == 10 or a == 11:
+#    print ('осень')
+#else:
+#    print ('ошибосик')
+
+#work 4
+
+#vvod = int(input('введите предложение с пробелами'))
+#for letter in vvod.items():
+ #   print(letter)
