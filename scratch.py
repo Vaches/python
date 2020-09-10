5cfee60bdf17d603c63497f4fd4d94a7f568fa5b
Vaches <70432569+Vaches@users.noreply.github.com>
2020-09-10 23:24:14 +0300

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,8 @@
+#1 задание
+my_file = open('tekst.txt', 'w')
+li = input('введите предлоение \n')
+while li:
+    my_file.whiteli (line)
+    line=input('введите предложение \n')
+    if not line:
+        break
